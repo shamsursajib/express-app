28a21c4e9cee4f9500acae5aecde75414837945a
shamsursajib <shamsur.sajib@outlook.com>
2021-10-26 18:22:31 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+    // res.send(' <p> Hello </p>')
+    res.sendFile('./views/index.html', { root: __dirname })
+})
+
+app.get('/add-item', (req, res) => {
+
+    res.sendFile('./views/add-item.html', { root: __dirname })
+})
+
+
+app.use((req, res) => {
+    res.sendFile('./views/error.html', { root: __dirname })
+
+}
+)
